a63b361d6f109ff27cf6c76a93329f53a87543d2
WubzyGD <wubzygd@gmail.com>
2022-02-02 16:11:19 -0700

    better logging

--- src/index.js
+++ src/index.js
@@ -4,16 +4,25 @@ const axios = require('axios');
  * Ping a website
  * @param website The website to ping
  * @param interval The interval per ping (in milli-seconds)
+ * @param logging Enable logging
+ * @param flipDM Flip the month/day to day/month in the log
+ * @param noURLFix If you're pinging a non-http/https url, set this to true
  * @example
  * const pinger = require("@hystleria/pinger")
  * pinger.ping("kie.lol", 60000, true)
  */
 
-const ping = async function (url, interval, logging) {
+const ping = async function (url, interval, logging=true, flipDM=false, noURLFix=false) {
     let count = 1;
     
     if (!interval) interval = 30000;
     if (!url) return console.log(`[🏓 @hystleria/pinger] You must specify a URL.`);
+
+    if (!url.startsWith('http://') && !url.startsWith('https://') && !noURLFix) {url = `https://${url}`;}
+
+    if (URLValidity(url) !== true || encodeURIComponent(url).includes("%3C" || "%3E" || "%20")) {
+        return console.log(`[🏓 @hystleria/pinger] You must specify a valid URL.`);
+    }
 
     if (logging == true){
         console.log(`[🏓 @hystleria/pinger] Currently logging pings for ${url} with the interval ${interval}.`);
@@ -29,18 +38,21 @@ const ping = async function (url, interval, logging) {
         return true;
     }
 
-    if (URLValidity(url) !== true || encodeURIComponent(url).includes("%3C" || "%3E" || "%20")) return console.log(`[🏓 @hystleria/pinger] You must specify a valid URL.`);
+    const ps = t=>`${t}`.padStart(2, '0');
+    const dm = (m,d)=>flipDM?`${ps(d)}/${ps(m)}`:`${ps(m)}/${ps(d)}`;
+
 
     setInterval(async () => {
-        const response = await axios.get(url, {
+        await axios.get(url, {
             headers: {
                 'User-Agent': '@hystleria/pinger'
             },
         })
+        .catch(e => console.log(`[🏓 @hystleria/pinger] [${dm(d.getMonth() + 1, d.getDate())} ${ps(d.getHours())}:${ps(d.getMinutes())}:${ps(d.getSeconds())}] Failed to ping ${url}. Error: `, e));
 
         if (logging === true) {
             const d = new Date();
-            console.log(`[🏓 @hystleria/pinger] [${d.getMonth()}/${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] Successfully pinged ${url} -> Ping#${count}`);
+            console.log(`[🏓 @hystleria/pinger] [${dm(d.getMonth() + 1, d.getDate())} ${ps(d.getHours())}:${ps(d.getMinutes())}:${ps(d.getSeconds())}] Successfully pinged ${url} -> Ping #${count}`);
         };
         
         count++;
